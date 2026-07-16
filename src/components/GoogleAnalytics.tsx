76fe86241eb5d5de1c0a1e1d32c02549916daf2b
AdrianHLuna <locielf@gmail.com>
2026-07-16 16:03:15 -0600

    Update GoogleAnalytics.tsx

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,14 +1,38 @@
 import Script from "next/script";
-import { doctor } from "@/data/doctor";
 
 export default function GoogleAnalytics() {
-  const gaId = doctor.ga4Id;
-  if (!gaId) return null;
-
   return (
     <>
+      {/* Google Tag Manager (GTM-N7NWM22N) */}
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N7NWM22N');`,
+        }}
+      />
+
+      {/* Google Ads (AW-18328333814) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18328333814"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18328333814');
+        `}
+      </Script>
+
+      {/* Google Analytics (G-2RGFS6YL97) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-2RGFS6YL97"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -16,9 +40,20 @@ export default function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${gaId}');
+          gtag('config', 'G-2RGFS6YL97');
         `}
       </Script>
+
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N7NWM22N"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
     </>
   );
 }
+
